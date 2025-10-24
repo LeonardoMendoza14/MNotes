62829b06925ad360393065fb9d012bf863f5063c
LeonardoMendoza <leodanielmp@gmail.com>
2025-10-24 00:52:02 -0600

    Initial Commit

--- app/src/main/java/com/mendoxy/mnotes/ui/MainActivity.kt
+++ app/src/main/java/com/mendoxy/mnotes/ui/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MNotesTheme {
-
+                Text("Hello")
             }
         }
     }
